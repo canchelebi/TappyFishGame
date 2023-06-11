9a62e92098f4b2f492ae7cac247611bf93e1bfce
Can Çelebi <cancleebi@gmail.com>
2023-06-11 12:06:19 +0300

    TappyFish #3
    Balığın hareketini sağlayacak script yazıldı

--- TappyFishAkademi/Assets/Scripts/Fish.cs
+++ TappyFishAkademi/Assets/Scripts/Fish.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     Rigidbody2D _rb;
+    [SerializeField]
+    private float _speed;
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -60;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         //_rb.gravityScale = 0;
-        _rb.velocity = new Vector2(_rb.velocity.x, 9f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            _rb.velocity = Vector2.zero;
+            _rb.velocity = new Vector2(_rb.velocity.x, _speed);
+        }
+        if (_rb.velocity.y > 0)
+        {
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+           
+        }
+        else if (_rb.velocity.y < 0)
+        {
+            if (angle > minAngle)
+            {
+                angle = angle - 2;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
-}
+} 
